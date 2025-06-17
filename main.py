e9d78ec2e4c22358e7dbd46a5ec01974d558e218
Opeyemi Adeyemo <adeyemoopeyemi@gmail.com>
2025-06-17 22:30:02 +0100

    code modified

--- main.py
+++ main.py
@@ -65,9 +65,14 @@ def go(config: DictConfig):
     )
             pass
 
-        # Filter rows within NYC latitude and longitude bounds
-        idx = df['longitude'].between(-74.25, -73.50) & df['latitude'].between(40.5, 41.2)
-        df = df[idx].copy()
+
+        # Filter out rows with prices outside min and max
+        idx_price = df['price'].between(args.min_price, args.max_price)
+        df = df[idx_price].copy()
+
+        # filter by longitude and latitude (NYC bounding box)
+        idx_geo = df['longitude'].between(-74.25, -73.50) & df['latitude'].between(40.5, 41.2)
+        df = df[idx_geo].copy()
 
         if "data_check" in active_steps:
             _ = mlflow.run(
